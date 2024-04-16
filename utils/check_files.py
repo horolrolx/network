450620b5d98a63f837d807a3a8222cd2209855c7
ten4roid <thdckdtjr8@gmail.com>
2024-04-16 10:10:09 +0900

    update

--- utils/check_files.py
+++ utils/check_files.py
@@ -1,13 +1,13 @@
 import os
 def get_file_list_prac():
-    file_list = [file for file in os.listdir("./중간고사/") if '.md' in file]
+    file_list = [file for file in os.listdir("./공부정리/") if '.md' in file]
     file_list = sorted(file_list)
     return file_list
   
 def make_info_prac(file_list):
     info = f"## 공부 정리\n\n"
     for file in file_list:
-        temp = f"- [{file}](https://github.com/xoxlo/computer_network/tree/main/중간고사/{file})\n"
+        temp = f"- [{file}](https://github.com/xoxlo/computer_network/tree/main/공부정리/{file})\n"
         info += temp
     return info
   
@@ -16,7 +16,7 @@ def make_read_me(info):
     
 # 컴퓨터 네트워크1
 
-공부 정리
+정리
 
 {info}
 """
