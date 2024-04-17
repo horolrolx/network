21251b5e9996a7e832bc87773eb08c6e3a68e370
ten4roid <thdckdtjr8@gmail.com>
2024-04-17 23:04:28 +0900

    update

--- utils/check_files.py
+++ utils/check_files.py
@@ -1,21 +1,57 @@
 import os
-def get_file_list_prac():
+def get_file_list_1():
+    file_list = [file for file in os.listdir("./computer_network/") if '.md' in file]
+    file_list = sorted(file_list)
+    return file_list
+  
+def make_info_1(file_list):
+    info = f"## 컴퓨터 네트워크 공부 정리\n\n"
+    for file in file_list:
+        temp = f"- [{file}](https://github.com/xoxlo/network/tree/main/computer_network/{file})\n"
+        info += temp
+    return info
+
+def get_file_list_2():
     file_list = [file for file in os.listdir("./공부정리/") if '.md' in file]
     file_list = sorted(file_list)
     return file_list
   
-def make_info_prac(file_list):
-    info = f"## 공부 정리\n\n"
+def make_info_2(file_list):
+    info = f"## 네트워크 특론 정리\n\n"
     for file in file_list:
-        temp = f"- [{file}](https://github.com/xoxlo/computer_network/tree/main/공부정리/{file})\n"
+        temp = f"- [{file}](https://github.com/xoxlo/network/tree/main/network_specialism/{file})\n"
+        info += temp
+    return info
+
+def get_file_list_3():
+    file_list = [file for file in os.listdir("./공부정리/") if '.md' in file]
+    file_list = sorted(file_list)
+    return file_list
+  
+def make_info_3(file_list):
+    info = f"## 컴퓨터 네트워크 중간고사 예상 문제\n\n"
+    for file in file_list:
+        temp = f"- [{file}](https://github.com/xoxlo/network/tree/main/computer_network/중간고사%20예상%20문제/{file})\n"
+        info += temp
+    return info
+
+def get_file_list_4():
+    file_list = [file for file in os.listdir("./공부정리/") if '.md' in file]
+    file_list = sorted(file_list)
+    return file_list
+  
+def make_info_4(file_list):
+    info = f"## 네트워크 특론 중간고사 예상 문제\n\n"
+    for file in file_list:
+        temp = f"- [{file}](https://github.com/xoxlo/network/tree/main/network_specialism/중간고사%20예상%20문제/{file})\n"
         info += temp
     return info
   
 def make_read_me(info):
     return f"""
     
-# 컴퓨터 네트워크1
-
+# 컴퓨터 네트워크
+# 네트워크 특론
 정리
 
 {info}
@@ -23,8 +59,11 @@ def make_read_me(info):
 
 
 def update_readme():
-    file_list_prac = get_file_list_prac()
-    info = make_info_prac(file_list_prac)
+    file_list1 = get_file_list_1()
+    file_list2 = get_file_list_2()
+    file_list3 = get_file_list_3()
+    file_list4 = get_file_list_4()
+    info = make_info_1(file_list1) + make_info_2(file_list2) + make_info_3(file_list3) + make_info_4(file_list4)
     readme = make_read_me(info)
     return readme
 
