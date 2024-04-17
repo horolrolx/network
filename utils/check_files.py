365af6416c2551bf21d6b44b44b67158a73a8fe8
ten4roid <46445480+xoxlo@users.noreply.github.com>
2024-04-17 23:12:09 +0900

    check_files.py 업데이트

--- utils/check_files.py
+++ utils/check_files.py
@@ -12,7 +12,7 @@ def make_info_1(file_list):
     return info
 
 def get_file_list_2():
-    file_list = [file for file in os.listdir("./공부정리/") if '.md' in file]
+    file_list = [file for file in os.listdir("./network_specialism/") if '.md' in file]
     file_list = sorted(file_list)
     return file_list
   
@@ -24,7 +24,7 @@ def make_info_2(file_list):
     return info
 
 def get_file_list_3():
-    file_list = [file for file in os.listdir("./공부정리/") if '.md' in file]
+    file_list = [file for file in os.listdir("./computer_network/중간고사%20예상%20문제/") if '.md' in file]
     file_list = sorted(file_list)
     return file_list
   
@@ -36,7 +36,7 @@ def make_info_3(file_list):
     return info
 
 def get_file_list_4():
-    file_list = [file for file in os.listdir("./공부정리/") if '.md' in file]
+    file_list = [file for file in os.listdir("./computer_specialism/중간고사%20예상%20문제/") if '.md' in file]
     file_list = sorted(file_list)
     return file_list
   
